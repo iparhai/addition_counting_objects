cb667558c9a0546251c6f240c470833e05ccaeb5
shaffanFast <k190863@nu.edu.pk>
2021-09-23 22:56:52 -0700

    css

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -156,13 +156,13 @@ class Quiz extends React.Component {
                 <tbody>
                   <tr >
                     {[...Array(parseInt(this.state.firstNumber))].map((e, i) => {
-                      return <td ><img key={i} src={this.state.randomImage} style={{ width: "100%", maxWidth:"150px"}} draggable="false"/> </td>
+                      return <td ><img key={i} src={this.state.randomImage} style={{ paddingLeft: "5%", width: "90%", maxWidth:"150px"}} draggable="false"/> </td>
                     })}
 
                     <td className="center" ><h1 style={{ fontSize: "3.5em" }}> {this.state.symbol} </h1></td>
 
                     {[...Array(parseInt(this.state.secondNumber))].map((e, i) => {
-                      return <td ><img key={i} src={this.state.randomImage} style={{ width : "100%", maxWidth:"150px"}}  draggable="false"/></td>
+                      return <td ><img key={i} src={this.state.randomImage} style={{ paddingLeft: "5%", width: "90%", maxWidth:"150px"}}  draggable="false"/></td>
                     })}
                   </tr>
                 </tbody>
